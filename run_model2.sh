9d12a80e4cb81921ab21543b573a19c996535435
skewondr <skewondr@naver.com>
2022-10-27 10:13:28 +0900

    debug attention_mask at augment_seq.py

--- run_model2.sh
+++ run_model2.sh
@@ -1,77 +1,61 @@
 #!/usr/bin/env bash
+# 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
 
-prob=(0.1 0.3 0.5 0.7 0.9)
-neg=(0 1)
-data=(algebra05 assistments09)
+data=(assistments09)
+neg=(0 1.0)
+prob=(0 1.0.3 0.5 0.7 0.9)
 
 for d in ${data[@]}; do
-    for p in ${prob[@]}; do
-        args=(
-            --model_name cl4kt
-            --data_name ${d}
-            --mask_prob ${p}
-            --crop_prob 0
-            --permute_prob 0
-            --replace_prob 0
-        )
-        echo `CUDA_VISIBLE_DEVICES=1 python main.py ${args[@]}`
-        args=(
-            --model_name cl4kt
-            --data_name ${d}
-            --mask_prob ${p}
-            --crop_prob 0
-            --permute_prob 0
-            --replace_prob 0
-            --choose_cl q_cl
-        )
-        echo `CUDA_VISIBLE_DEVICES=1 python main.py ${args[@]}`
-        for n in ${neg[@]}; do
+    for c in ${neg[@]}; do
+        for p in ${prob[@]}; do
             args=(
                 --model_name cl4kt
                 --data_name ${d}
+                --choose_cl s_cl
                 --mask_prob ${p}
                 --crop_prob 0
                 --permute_prob 0
                 --replace_prob 0
-                --negative_prob ${n}
-                --choose_cl s_cl
+                --negative_prob ${c}
             )
             echo `CUDA_VISIBLE_DEVICES=1 python main.py ${args[@]}`
         done
-    done
-done
-
-for d in ${data[@]}; do
-    for p in ${prob[@]}; do
-        args=(
-            --model_name cl4kt
-            --data_name ${d}
-            --mask_prob 0
-            --crop_prob ${p}
-            --permute_prob 0
-            --replace_prob 0
-        )
-        echo `CUDA_VISIBLE_DEVICES=1 python main.py ${args[@]}`
-        args=(
-            --model_name cl4kt
-            --data_name ${d}
-            --mask_prob 0
-            --crop_prob ${p}
-            --permute_prob 0
-            --replace_prob 0
-            --choose_cl q_cl
-        )
-        echo `CUDA_VISIBLE_DEVICES=1 python main.py ${args[@]}`
-        for n in ${neg[@]}; do
+        for p in ${prob[@]}; do
             args=(
                 --model_name cl4kt
                 --data_name ${d}
+                --choose_cl s_cl
                 --mask_prob 0
                 --crop_prob ${p}
                 --permute_prob 0
                 --replace_prob 0
-                --negative_prob ${n}
+                --negative_prob ${c}
+            )
+            echo `CUDA_VISIBLE_DEVICES=1 python main.py ${args[@]}`
+        done
+        for p in ${prob[@]}; do
+            args=(
+                --model_name cl4kt
+                --data_name ${d}
                 --choose_cl s_cl
+                --mask_prob 0
+                --crop_prob 0
+                --permute_prob ${p}
+                --replace_prob 0
+                --negative_prob ${c}
+            )
+            echo `CUDA_VISIBLE_DEVICES=1 python main.py ${args[@]}`
+        done
+        for p in ${prob[@]}; do
+            args=(
+                --model_name cl4kt
+                --data_name ${d}
+                --choose_cl s_cl
+                --mask_prob 0
+                --crop_prob 0
+                --permute_prob 0
+                --replace_prob ${p}
+                --negative_prob ${c}
             )
             echo `CUDA_VISIBLE_DEVICES=1 python main.py ${args[@]}`
         done
