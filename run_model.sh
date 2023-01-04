f956718a17a17c7a523301b8ccbd8a588a32d7b8
skewondr <skewondr@naver.com>
2023-01-05 05:12:46 +0900

    run bash

--- run_model.sh
+++ run_model.sh
@@ -1,13 +1,23 @@
 #!/usr/bin/env bash
 # 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
 
-data=(algebra05 assistments09)
+data=(algebra05)
+de=(rde sde lsde)
+k=(1 5 10 25 100)
 
 for d in ${data[@]}; do
-    args=(
-        --model_name sakt
-        --data_name ${d}
-        --describe full_len
-    )
-    echo `CUDA_VISIBLE_DEVICES=0 python main.py ${args[@]}`
+    echo `CUDA_VISIBLE_DEVICES=0 python main.py --model_name sakt --data_name ${d} --de_type none_0 --describe diff_encoding`
+    echo `CUDA_VISIBLE_DEVICES=0 python main.py --model_name sakt --data_name ${d} --de_type rde_1000 --describe diff_encoding`
+    for e in ${de[@]}; do
+        for kk in ${k[@]}; do
+            args=(
+                --model_name sakt
+                --data_name ${d}
+                --de_type ${e}_${kk}
+                --describe diff_encoding
+            )
+            echo `CUDA_VISIBLE_DEVICES=0 python main.py ${args[@]}`
+        done
+    done
 done
+
