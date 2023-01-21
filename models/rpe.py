fdad9f48b3d3c4d7a32591c197bad82d453919cc
skewondr <skewondr@naver.com>
2023-01-21 17:58:40 +0900

    new alibi --> sakt

--- models/rpe.py
+++ models/rpe.py
@@ -270,6 +270,90 @@ class ALiBiPositionalEmbeddings(nn.Module):
         _future_mask = _future_mask.to(tensor)
         return _future_mask[:tensor.shape[0]*self.attn_heads, :dim, :dim]
 
+    def buffered_future_mask_sakt(self, tensor, diff=None):
+        """
+        !!!batch_size, attn_heads, max_len, max_len 을 만들 때, !!!
+        attn_heads -> batch 순으로 repeat해나가야 함.
+        """
+        diff1 = diff[:, 1:]
+        diff2 = diff[:, :-1]
+        _future_mask = torch.triu(
+            self.fill_with_neg_inf(torch.zeros([self.max_len, self.max_len])), 1 #1, max_len, max_len 
+        ).unsqueeze(0).unsqueeze(0) 
+        dim = tensor.size(2)
+        if "0" in self.de and diff is not None:
+            print("0")
+            """서로 먼 위치의 attention score의 영향력을 상대적으로 낮게 부여."""
+            _future_mask = _future_mask + self.alibi[:,:,:-1].unsqueeze(0).repeat(tensor.shape[0], 1, 1, 1) #(1, 1, max_len, max_len) + (batch_size, attn_heads, 1, max_len) 
+        if "1" in self.de and diff is not None:
+            print("1")
+            """서로 비슷한 난이도의 attention score의 영향력을 상대적으로 높게 부여: 난이도 차이"""
+            x1 = diff1.unsqueeze(-1).expand(-1, -1, self.max_len)
+            x2 = diff2.unsqueeze(-1).expand(-1, -1, self.max_len).transpose(-1, -2).contiguous()
+            diff_effect = torch.squeeze((self.token_num+1)-torch.abs(x1- x2)[None, None, :, :].type(
+                torch.FloatTensor
+            ))  # [1, 1, seqlen, seqlen]
+            diff_effect = diff_effect.float().to(tensor.get_device())
+            # [batch_size, 8, seqlen, seqlen] positive distance
+            # dist_score => d(t, tau)
+            # diff_effect -= torch.diag(torch.ones(self.max_len)*(self.token_num+1)) #batch_size, max_len, max+len 
+            dist_scores = torch.where(diff_effect>(self.token_num+1)*0.5, diff_effect.double(), 0.).to(tensor.get_device())
+            dist_scores = dist_scores.unsqueeze(1).repeat(1, self.attn_heads, 1, 1)  # batch_size, attn_heads, 1, max_len
+            dist_scores *= self.slopes.unsqueeze(0).repeat(tensor.shape[0], 1, 1, 1) 
+            _future_mask = _future_mask + dist_scores #batch_size*attn_heads, max_len, max_len 
+        if "2" in self.de and diff is not None:
+            print("2")
+            """서로 비슷한 난이도의 attention score의 영향력을 상대적으로 높게 부여: 내적값"""
+            """답변에 따른 대칭적 난이도의 차가 클수록 attention score의 영향력을 상대적으로 높게 부여."""
+            x1 = self.diff_emb(diff1)
+            x1 /= x1.norm(dim=-1, keepdim=True)
+            x2 = self.diff_emb(diff2)
+            x2 /= x2.norm(dim=-1, keepdim=True)
+            diff_effect = x1@x2.transpose(-1, -2).to(tensor.get_device())
+            # [batch_size, 8, seqlen, seqlen] positive distance
+            # dist_score => d(t, tau)
+            # diff_effect -= torch.diag(torch.ones(self.max_len))
+            dist_scores = torch.where(diff_effect>0.5, diff_effect.double(), 0.).to(tensor.get_device())
+            dist_scores = dist_scores.unsqueeze(1).repeat(1, self.attn_heads, 1, 1)  # batch_size, attn_heads, 1, max_len
+            dist_scores *= self.slopes.unsqueeze(0).repeat(tensor.shape[0], 1, 1, 1)*100 
+            _future_mask = _future_mask + dist_scores #batch_size, attn_heads, max_len, max_len 
+        if "3" in self.de and diff is not None:
+            print("3")
+            """높은 난이도의 attention score의 영향력을 상대적으로 높게 부여."""
+            x1 = ((self.token_num+1)-diff1).unsqueeze(1).repeat(1, self.max_len, 1)
+            x2 = ((self.token_num+1)-diff2).unsqueeze(1).repeat(1, self.max_len, 1).transpose(-1, -2).contiguous()
+            diff_effect = (x1 + x2)/2
+            diff_effect = torch.squeeze(diff_effect[None, None, :, :].type(
+                torch.FloatTensor
+            ))  # [1, 1, seqlen, seqlen]
+            diff_effect = diff_effect.float()
+            # [batch_size, 8, seqlen, seqlen] positive distance
+            # dist_score => d(t, tau)
+            dist_scores = torch.where(diff_effect>(self.token_num+1)*0.5, diff_effect.double(), 0.).to(tensor.get_device())
+            dist_scores = dist_scores.unsqueeze(1).repeat(1, self.attn_heads, 1, 1)  # batch_size, attn_heads, 1, max_len
+            dist_scores *= self.slopes.unsqueeze(0).repeat(tensor.shape[0], 1, 1, 1) 
+            _future_mask = _future_mask + dist_scores #batch_size, attn_heads, max_len, max_len 
+        if "4" in self.de and diff is not None:
+            print("4")
+            """등장 빈도가 낮은 난이도의 attention score의 영향력을 상대적으로 높게 부여."""
+            bins = f.normalize(torch.bincount(torch.flatten(diff)).float(), dim =0)
+            diff_freq = torch.gather(bins.repeat(tensor.shape[0], self.max_len+1, 1), -1, diff.unsqueeze(-1)).squeeze()
+            x1 = (1-diff_freq[:, 1:]).unsqueeze(1).repeat(1, self.max_len, 1) #(batch_size, max_len) ->
+            x2 = (1-diff_freq[:, :-1]).unsqueeze(1).repeat(1, self.max_len, 1).transpose(-1, -2).contiguous()
+            diff_effect = (x1 + x2)/2
+            diff_effect = torch.squeeze(diff_effect[None, None, :, :].type(
+                torch.FloatTensor
+            ))  # [1, 1, seqlen, seqlen]
+            diff_effect = diff_effect.float()
+            # [batch_size, 8, seqlen, seqlen] positive distance
+            # dist_score => d(t, tau)
+            dist_scores = torch.where(diff_effect>0.5, diff_effect.double(), 0.).to(tensor.get_device())
+            dist_scores = dist_scores.unsqueeze(1).repeat(1, self.attn_heads, 1, 1)  # batch_size, attn_heads, 1, max_len
+            dist_scores *= self.slopes.unsqueeze(0).repeat(tensor.shape[0], 1, 1, 1)*100 
+            _future_mask = _future_mask + dist_scores #batch_size, attn_heads, max_len, max_len 
+        
+        _future_mask = _future_mask.to(tensor)
+        return _future_mask[:tensor.shape[0]*self.attn_heads, :dim, :dim]
 
     def fill_with_neg_inf(self, t):
         """FP16-compatible function that fills a tensor with -inf."""
